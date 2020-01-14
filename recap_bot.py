508d1fdb69c96b2f9a931c18e20b65ad81c0ac78
Asher <gilaniasher@gmail.com>
2020-01-13 22:41:36 -0500

    Client has correct credentials since Bot succesfully retrieves title and text from hot posts in the subreddit.

--- recap_bot.py
+++ recap_bot.py
@@ -3,3 +3,8 @@ import praw
 # Create a Reddit instance using the configuration [recap-bot] from the praw.ini file
 reddit = praw.Reddit('recap-bot')
 subreddit = reddit.subreddit('testingground4bots')
+
+for entry in subreddit.hot(limit=5):
+    print("Title: ", entry.title)
+    print("Text: ", entry.selftext)
+    print("________________________\n")
