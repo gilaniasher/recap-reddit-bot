6a772069c9e7f3aea8418eea3d6d0d7f28826ffe
Asher <gilaniasher@gmail.com>
2020-01-16 01:45:21 -0500

    Bot now remembers comments it replies to so that it does not reply to them multiple times; Adjusted way link is captured

--- recap_bot.py
+++ recap_bot.py
@@ -6,29 +6,30 @@ import smmry_credentials
 # Create a Reddit instance using the configuration [recap-bot] from the praw.ini file
 reddit = praw.Reddit('recap-bot')
 subreddit = reddit.subreddit('testingground4bots')
+recap_user = reddit.user.me()
 
 keyphrase = '!recapbot '
 
 # Check to see if the keyphrase is in any comments
 for comment in subreddit.stream.comments():
-    if comment.body.startswith(keyphrase):
-        # Remove the keyphrase from the bot request
+    if comment.body.startswith(keyphrase) and (comment not in recap_user.saved(limit=None)): 
+        print(comment.body)
+
+        # Save the comment so the bot does not reply to it later
+        comment.save()
+
+        # Use regex to extract the link from the request
         match = re.search(r'\[.*\]\((.*)\)', comment.body)
+        link = match.group(1) if match else comment.body.replace(keyphrase, '')
 
-        if match:
-            link = match.group(1)
-            
-            # Make API call to SMMRY using API key
-            params = {'SM_API_KEY': smmry_credentials.api_key, 'SM_URL': link}
-            res = requests.get(url='https://api.smmry.com', params=params).json()
+        # Make API call to SMMRY using API key
+        params = {'SM_API_KEY': smmry_credentials.api_key, 'SM_URL': link}
+        res = requests.get(url='https://api.smmry.com', params=params).json()
 
-            # Reply to user request
-            if 'sm_api_content' in res:
-                comment.reply(res['sm_api_content'])
-            else:
-                comment.reply('Invalid request')
-
-            print('Replied!')
+        # Reply to user request
+        if 'sm_api_content' in res:
+            comment.reply(res['sm_api_content'])
         else:
-            comment.reply('No link found in comment')
-            print('Replied')
+            comment.reply('Invalid request')
+
+        print('Replied!')
